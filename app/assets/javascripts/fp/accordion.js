2e82683ff72db8deaad80de21daef02c4335aca9
Linz Jax <linzjax@gmail.com>
2016-02-19 11:08:05 -0500

    pinned make atlas button to the bottom

--- app/assets/javascripts/fp/accordion.js
+++ app/assets/javascripts/fp/accordion.js
@@ -3,24 +3,18 @@ Javascript for accordion menu on /compose
 */
 
 $(document).on('page:change', function(){
-    $('.accordion-header').click(function(e){
-        var $selected = $(this).closest('.accordion-group');
-        e.preventDefault();
-        if ($selected.hasClass('selected-group')){
-            $selected.removeClass('selected-group');
-        } else {
-            $('.selected-group').removeClass('selected-group');
-            $selected.addClass('selected-group');
-        }
-        var $box = ($(this).children('.box'));
-        
-        if ( $box.children().hasClass('up-arrow') ) {
-            $box.children().removeClass('up-arrow').addClass('down-arrow');
-        } else {
-            $('.up-arrow').removeClass('up-arrow').addClass('down-arrow');
-            $box.children().removeClass('down-arrow').addClass('up-arrow');
-        }
-    });
+    function toggle()
+    {
+        $(".tab-content").addClass("activated").toggleClass("active");
+    }
+
+    $(function()
+      {
+        $(".tab-content").on("transitionend", function() {
+            $(this).removeClass("activated");
+        });
+      }
+    )();
 
     $(function(){
         $('#atlas_provider').selectize({
